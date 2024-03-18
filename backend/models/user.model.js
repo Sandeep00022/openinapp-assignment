e3e1f0491ef33ef6a38da1b6aa17a8f0862dd10b
Sandeep Yadav <97525395+Sandeep00022@users.noreply.github.com>
2024-03-18 14:18:04 +0530

    feat: Initial setup of backend Node.js application

---
+++ backend/models/user.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  phone_number: {
+    type: Number,
+    required: true,
+  },
+  priority: {
+    type: [Number],
+    enum: [0, 1, 2],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
